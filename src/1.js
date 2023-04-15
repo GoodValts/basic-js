0fdcfe53741988a4a7d066a1c1c9ef827d0d164b
GoodValts <martiniukgomel@gmail.com>
2023-04-15 13:39:46 +0300

    dream-team done

--- src/1.js
+++ src/1.js
@@ -1,4 +1,6 @@
-let arrCats = [[0, 1, '^^'], [0, '^^', 2, [0, '^^']], ['^^', 1, 2]];
+        //CountCats
+
+/*let arrCats = [[0, 1, '^^'], [0, '^^', 2, [0, '^^']], ['^^', 1, 2]];
 
 let cnt = 0;
 
@@ -17,4 +19,48 @@ for(i=0; i<arrCh.length; i++){
     }
 }
 
-console.log('cnt=', cnt)
+console.log('cnt=', cnt)*/
+
+        //DreamTeam
+
+let arr = [
+    '   William Alston ',
+    ' Paul Benacerraf',
+    '  Ross Cameron',
+    '       Gilles Deleuze',
+    '  Arda Denkel ',
+    '  Michael Devitt',
+    '  Kit Fine',
+    ' Nelson Goodman',
+    'David Kolb',
+    '   Saul Kripke',
+    '  Trenton Merricks',
+    '  Jay Rosenberg',
+  ]
+
+
+
+  if(typeof(arr) != 'object'){
+    return false;
+  } else {
+    console.log(typeof(arr));
+
+    let namesArr = [];
+
+    for(i=0; i<arr.length; i++){
+        if(typeof(arr[i]) == 'string'){
+            console.log('arr.i=', arr[i]);
+            namesArr.push(arr[i].replaceAll(' ', '').slice(0,1).toUpperCase())
+        } else {
+            console.log('not name');
+        }
+    }
+
+    console.log('namesArr=', namesArr);
+
+    let str = namesArr.sort().join('');
+
+    console.log('str=', str);
+
+    return str;
+  }
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(arr) {
+  if(typeof(arr) != 'object' || arr === null){
+    return false;
+  } else {
+    console.log(typeof(arr));
+
+    let namesArr = [];
+
+    for(i=0; i<arr.length; i++){
+        if(typeof(arr[i]) == 'string'){
+            console.log('arr.i=', arr[i]);
+            namesArr.push(arr[i].replaceAll(' ', '').slice(0,1).toUpperCase())
+        } else {
+            console.log('not name');
+        }
+    }
+
+    console.log('namesArr=', namesArr);
+
+    let str = namesArr.sort().join('');
+
+    console.log('str=', str);
+
+    return str;
+  }
 }
 
 module.exports = {
