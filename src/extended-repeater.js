84d9845597b683d0d6390e0c708a13ea3ed019d4
GoodValts <martiniukgomel@gmail.com>
2023-04-18 01:22:10 +0300

    add ext separ

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+  function checkVal(el){
+    if(el != undefined && typeof(el) != 'number'){
+        return el.toString();
+    } else if (el > 0 && typeof(el) === 'number'){
+        return el;
+    } else {
+        if (typeof(el) === 'number'){
+            return 1;
+        } else {
+            return '';
+        }
+    }
+  }
+
+  str = checkVal(str);
+  repeatTimes = checkVal(options.repeatTimes);
+  separator = checkVal(options.separator);
+  addition = checkVal(options.addition);
+  additionRepeatTimes = checkVal(options.additionRepeatTimes);
+  additionSeparator = checkVal(options.additionSeparator);
+
+  if (separator.length === 0){
+    separator = '+';
+  }
+  if (additionSeparator.length === 0){
+    additionSeparator = '+';
+  }
+  
+  function repeat(str, times, separator){
+    let newStr = str;
+    for(i=0; i<(times-1); i++){
+        newStr = newStr + separator + str;
+        console.log(newStr);
+        console.log('--');
+    }
+    return newStr;
+  }
+
+  str = str + repeat(addition, additionRepeatTimes, additionSeparator);
+  return repeat(str, repeatTimes, separator)
 }
 
 module.exports = {
